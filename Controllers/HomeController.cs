b0e1ba81bdf7b996d909b1b755467845763530ac
David Alin <davidalin9182@gmail.com>
2023-07-23 02:18:42 +0300

    inital

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Proiect_HCI.Models;
+using System.Diagnostics;
+
+namespace Proiect_HCI.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult AboutUs()
+        {
+            return View();
+        }
+
+		public IActionResult Itinerary()
+		{
+			return View();
+		}
+
+		public IActionResult Search()
+		{
+			return View();
+		}
+		public IActionResult Profile()
+		{
+			return View();
+		}
+
+		public IActionResult _SignUp()
+		{
+			return PartialView();
+		}
+		public IActionResult _SignIn()
+		{
+			return PartialView();
+		}
+
+
+		[ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Proiect_HCI.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
